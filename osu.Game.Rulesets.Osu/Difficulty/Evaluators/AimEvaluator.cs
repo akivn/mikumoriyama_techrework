565745ae34608b13be82aac78e1db110eb0fb55c
akivn <patrickkeroro226@gmail.com>
2024-12-05 20:23:54 +0000

    Update AimEvaluator.cs

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -101,11 +101,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                     wideAngleBonus *= angleBonus * (1 - Math.Min(wideAngleBonus, Math.Pow(calcWideAngleBonus(lastAngle), 3)));
                     // Penalize acute angles if they're repeated, reducing the penalty as the lastLastAngle gets more obtuse.
                     acuteAngleBonus *= 0.5 + 0.5 * (1 - Math.Min(acuteAngleBonus, Math.Pow(calcAcuteAngleBonus(lastLastAngle), 3)));
-                    // Angle deviation bonus and Wiggle bonus rewards jumps with angle variance and speed change, impacting aim control, alternating and tech maps. ADB is capped at 0.15.
+                    // Angle deviation bonus and Wiggle bonus rewards jumps with angle variance and speed change, impacting aim control, alternating and tech maps.
                     angleDeviationBonus *= Math.Max(150 / Math.Max(osuCurrObj.StrainTime, 25) - 1, 0) // Buff starts at 200bpm 1/2s.
                                         * (currVelocity != 0 ? ((Math.PI - Math.Abs(currAngle)) / Math.PI) : 0); // Buff based on Velocity change and nerf if it is obtuse angle or overlap cheeses.
-                    if (angleDeviationBonus > 0.15)
-                        angleDeviationBonus = 0.15;
                     wiggleBonus *= Math.Max(150 / Math.Max(osuCurrObj.StrainTime, 62.5) - 1, 0) // Buff starts at 200bpm 1/2s and caps at 240bpm 1/4s.
                                 * Math.Min(1 + osuCurrObj.LazyJumpDistance / 120, 2) // Buff based on LazyJumpDistance.
                                 * ((Math.Max((prevVelocity + 0.7) / (currVelocity + 0.7), (currVelocity + 0.7) / (prevVelocity + 0.7))) - 0.95) * Math.Min(Math.Pow(Math.Min(prevVelocity, currVelocity), 3), 1) // Buff based on velocity change and initial velocity, capping at 1o!px/ms (Burning Star).
